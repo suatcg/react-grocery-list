034428715256820ab7dc0b521bc54dbbc314fc0c
suatcg <suat-can_24@hotmail.com>
2023-04-06 17:26:32 +0300

    search refactored

--- src/Component/Searchbar.js
+++ src/Component/Searchbar.js
@@ -1,7 +1,8 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './SearchBar.css';
+import { GiSlicedBread } from 'react-icons/gi';
 
 import debounce from '../utils/debounce';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,37 +15,38 @@ import { deleteHistory } from '../../storeRTK/searchSlice';
 const Searchbar = () => {
 	const dispatch = useDispatch();
 	const ref = useRef();
+	const asideRef = useRef();
 	const handleSubmit = (e) => e.preventDefault();
 
-	const displayInputHandler = (e) => {
+	const displayInputHandler = () => {
 		ref.current.style.display = 'block';
+		dispatch(fetchCategory());
+	};
+	const blurInputHandler = () => {
+		ref.current.style.display = 'none';
+		asideRef.current.style.maxHeight = 0;
 	};
 	const { category, status, error } = useSelector(selectAllCategory);
 
-	const [searchResult, setSearchResults] = useState(null);
+	let content;
 
-	const handleSearchChange = (category, status, error, value) => {
-		// if (!e.target.value) return setSearchResults(posts);
-		// const resultsArray = posts.filter(
-		// 	(post) =>
-		// 		post.title.includes(e.target.value) ||
-		// 		post.body.includes(e.target.value)
-		// );
-		// setSearchResults(resultsArray);
-
-		dispatch(fetchCategory());
-
-		console.log(category, status, error, value);
+	const handleSearchChange = (category, _, _2, value) => {
+		asideRef.current.style.maxHeight = '500px';
 
 		const matchedItems = Object.values(category)
 			.flat()
-			.filter((product) => product.name.includes(value));
+			.filter((product) => value.length > 1 && product.name.includes(value));
+		// console.log(matchedItems);
 
 		if (matchedItems.length) {
-			setSearchResults(matchedItems);
+			content = matchedItems;
+		} else if (value.length === 0) {
+			content = `Type product that you want to add list.`;
 		} else {
-			setSearchResults(value);
+			content = `Produt is not found by this '${value}' keyword.`;
 		}
+
+		console.log(content);
 	};
 
 	const optimizedFunc = useCallback(
@@ -54,21 +56,38 @@ const Searchbar = () => {
 
 	return (
 		// custom field
-		<form className="search" onSubmit={handleSubmit}>
-			<input
-				className="search__input"
-				type="text"
-				id="search"
-				onChange={(e) => optimizedFunc(e.target.value)}
-				ref={ref}
-			/>
-			<button className="search__button" onClick={displayInputHandler}>
-				<FontAwesomeIcon
-					icon={faMagnifyingGlass}
-					className="search-button--icon"
+		<>
+			<form className="search" onSubmit={handleSubmit}>
+				<input
+					className="search__input"
+					type="text"
+					id="search"
+					onChange={(e) => optimizedFunc(e.target.value)}
+					placeholder="Search for a product..."
+					onBlur={blurInputHandler}
+					ref={ref}
 				/>
-			</button>
-		</form>
+				<button className="search__button" onClick={displayInputHandler}>
+					<FontAwesomeIcon
+						icon={faMagnifyingGlass}
+						className="search-button--icon"
+					/>
+				</button>
+				<aside ref={asideRef} id="search-results-container">
+					{/* your search results will go here  */}
+					{Array.isArray(content) &&
+						content.map((el) => {
+							return (
+								<>
+									<svg></svg>
+									<p></p>
+								</>
+							);
+						})}
+					<p>alksdjflakjsdf</p>
+				</aside>
+			</form>
+		</>
 	);
 };
 
